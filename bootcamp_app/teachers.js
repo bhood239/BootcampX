be6103ac8e3aee13d1d81b9119550ec3bfe0db3a
Ben Hood <benhood97@btinternet.com>
2024-04-11 20:17:45 -0700

    parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,20 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = $1
+ORDER BY teacher;
+`
+const values = [process.argv[2]]
+
 pool
   .query(
-    `
-  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-  FROM teachers
-  JOIN assistance_requests ON teacher_id = teachers.id
-  JOIN students ON student_id = students.id
-  JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name = '${process.argv[2]}'
-  ORDER BY teacher;
-`
+    queryString, values
   )
   .then((res) => {
     res.rows.forEach((row) => {
@@ -27,3 +30,5 @@ pool
     });
   })
   .catch((err) => console.error("query error", err.stack));
+
+  
